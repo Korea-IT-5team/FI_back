93b06104fd7c4256a0d318651eb1e1c72d34d5fd
OREO206 <baek6654@naver.com>
2024-05-27 12:13:42 +0900

    240527 notice board UPDATE

--- src/main/java/com/project/back/repository/NoticeBoardRepository.java
+++ src/main/java/com/project/back/repository/NoticeBoardRepository.java
@@ -1,5 +1,7 @@
 package com.project.back.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import com.project.back.entity.NoticeBoardEntity;
 
 @Repository
 public interface NoticeBoardRepository extends JpaRepository<NoticeBoardEntity,Integer> {
-    
+    NoticeBoardEntity findByNoticeNumber(Integer noticeNumber);
+    List<NoticeBoardEntity> findByOrderByNoticeNumberDesc();
+    List<NoticeBoardEntity> findByNoticeTitleContainsOrderByNoticeNumberDesc(String noticeTitle);
 }
