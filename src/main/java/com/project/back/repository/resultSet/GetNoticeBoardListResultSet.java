50b57860966f015a88af4a6e53e1b2e24ca4996d
OREO206 <baek6654@naver.com>
2024-06-05 10:18:58 +0900

    240604

--- src/main/java/com/project/back/repository/resultSet/GetNoticeBoardListResultSet.java
+++
@@ -1,9 +0,0 @@
-package com.project.back.repository.resultSet;
-
-public interface GetNoticeBoardListResultSet {
-  Integer getNoticeNumber();
-  String getNoticeTitle();
-  String getNoticeWriterNickname();
-  String getNoticeWriteDatetime();
-  Integer getNoticeViewCount();
-}
--- src/main/java/com/project/back/repository/resultSet/GetRestaurantFavoriteItemResultSet.java
+++
@@ -1,9 +0,0 @@
-package com.project.back.repository.resultSet;
-
-public interface GetRestaurantFavoriteItemResultSet {
-    Integer getRestaurantId();
-    String getRestaurantImage();
-    String getRestaurantName();
-    String getRestaurantFoodCategory();
-    String getRestaurantLocation();
-}
