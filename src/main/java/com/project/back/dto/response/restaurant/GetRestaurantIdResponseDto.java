aea09b98e37d25e358b692bee924e1e2b4cc52b1
pjh753156a <00-g-00@naver.com>
2024-06-17 14:25:28 +0900

    240617 controller dto service update

---
+++ src/main/java/com/project/back/dto/response/restaurant/GetRestaurantIdResponseDto.java
@@ -0,0 +1,27 @@
+package com.project.back.dto.response.restaurant;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import com.project.back.dto.response.ResponseCode;
+import com.project.back.dto.response.ResponseDto;
+import com.project.back.dto.response.ResponseMessage;
+import com.project.back.entity.RestaurantEntity;
+
+import lombok.Getter;
+
+@Getter
+public class GetRestaurantIdResponseDto extends ResponseDto
+{
+    private Integer restaurantId; 
+
+    private GetRestaurantIdResponseDto(RestaurantEntity restaurantEntity)
+    throws Exception {
+        super(ResponseCode.SUCCESS, ResponseMessage.SUCCESS);
+        this.restaurantId = restaurantEntity.getRestaurantId();
+    }
+
+    public static ResponseEntity<GetRestaurantIdResponseDto> success(RestaurantEntity restaurantEntity)
+    throws Exception {
+        GetRestaurantIdResponseDto responseBody = new GetRestaurantIdResponseDto(restaurantEntity);
+        return ResponseEntity.status(HttpStatus.OK).body(responseBody);
+    }
+}
