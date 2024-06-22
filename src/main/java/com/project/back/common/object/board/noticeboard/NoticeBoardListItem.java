9334f5c32b62a47421aa50f214ac43e32cdf2b88
roqkfwk87 <tuicswrjj86874@naver.com>
2024-06-22 20:35:22 +0900

    2020

--- src/main/java/com/project/back/common/object/board/noticeboard/NoticeBoardListItem.java
+++ src/main/java/com/project/back/common/object/board/noticeboard/NoticeBoardListItem.java
@@ -16,12 +16,12 @@ public class NoticeBoardListItem {
     private String noticeWriteDatetime;
     private Integer viewCount;
 
-    private NoticeBoardListItem(GetNoticeBoardListResultSet resultSets) throws Exception {
-        this.noticeNumber = resultSets.getNoticeNumber();
-        this.noticeTitle = resultSets.getNoticeTitle();
-        this.noticeWriterNickname = resultSets.getNoticeWriterNickname();
+    private NoticeBoardListItem(GetNoticeBoardListResultSet resultSet) throws Exception {
+        this.noticeNumber = resultSet.getNoticeNumber();
+        this.noticeTitle = resultSet.getNoticeTitle();
+        this.noticeWriterNickname = resultSet.getNoticeWriterNickname();
 
-        String writeDate = ChangeDateFormatUtil.changeYYMMDD(resultSets.getNoticeWriteDatetime());
+        String writeDate = ChangeDateFormatUtil.changeYYMMDD(resultSet.getNoticeWriteDatetime());
         this.noticeWriteDatetime = writeDate;
 
         this.viewCount = resultSet.getViewCount();
